10236ab15af38a0607af66d09986aed2b3cbf009
tarikumola <97729261+tarikumola@users.noreply.github.com>
2022-04-01 21:16:14 +0300

    Update requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,4 @@
-const API_KEY="7abc99ee77e8f993d57232eb99399ce8";
-
+const API_KEY=process.env.REACT_APP_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
